1ec443c7274b6f9fb6048f5872e2eec3e8f9704b
Elif Bodur <108813775+ElifBodur@users.noreply.github.com>
2022-11-02 14:35:23 +0300

    Add files via upload

---
+++ ödev2 - loginscreen/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ödev2___loginscreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.tboxSifre = new System.Windows.Forms.TextBox();
+            this.tboxKa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSonuç = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.Location = new System.Drawing.Point(219, 169);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(101, 32);
+            this.btnGiris.TabIndex = 0;
+            this.btnGiris.Text = "Giriş Yap";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // tboxSifre
+            // 
+            this.tboxSifre.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.tboxSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tboxSifre.Location = new System.Drawing.Point(199, 114);
+            this.tboxSifre.Name = "tboxSifre";
+            this.tboxSifre.Size = new System.Drawing.Size(140, 30);
+            this.tboxSifre.TabIndex = 1;
+            this.tboxSifre.UseSystemPasswordChar = true;
+            // 
+            // tboxKa
+            // 
+            this.tboxKa.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.tboxKa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tboxKa.Location = new System.Drawing.Point(199, 60);
+            this.tboxKa.Name = "tboxKa";
+            this.tboxKa.Size = new System.Drawing.Size(140, 30);
+            this.tboxKa.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(63, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(125, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Şifre ";
+            // 
+            // lblSonuç
+            // 
+            this.lblSonuç.AutoSize = true;
+            this.lblSonuç.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuç.Location = new System.Drawing.Point(440, 119);
+            this.lblSonuç.Name = "lblSonuç";
+            this.lblSonuç.Size = new System.Drawing.Size(64, 25);
+            this.lblSonuç.TabIndex = 5;
+            this.lblSonuç.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 355);
+            this.Controls.Add(this.lblSonuç);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tboxKa);
+            this.Controls.Add(this.tboxSifre);
+            this.Controls.Add(this.btnGiris);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.TextBox tboxSifre;
+        private System.Windows.Forms.TextBox tboxKa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSonuç;
+    }
+}
+
---
+++ ödev2 - loginscreen/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev2___loginscreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            string ka = tboxKa.Text;
+            string sifre = tboxSifre.Text;
+
+            if (ka== "Aylin")
+            {
+                if (sifre== "12345")
+                {
+                    lblSonuç.Text = "Giriş Başarılı";
+                }
+                else
+                {
+                    lblSonuç.Text = "Şifreniz Yanlış";
+                }
+            }
+            else
+            {
+                lblSonuç.Text = "Kullanıcı adınız yanlış";
+            }
+        }
+    }
+}
